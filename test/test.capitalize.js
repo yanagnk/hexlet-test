dc9b5fca4f49fb3bad99ad92558aacde6db1964e
Yana Gunko <yana.gunko@gmail.com>
2023-08-28 13:17:16 +0200

    added asserts lesson

--- test/test.capitalize.js
+++ test/test.capitalize.js
@@ -1,11 +1,16 @@
+import { strict as assert } from 'node:assert';
+
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
+/* if (capitalize('hello') !== 'Hello') {
     throw new Error('Функция работает неверно!');
   }
   
   if (capitalize('') !== '') {
     throw new Error('Функция работает неверно!');
-  }
+  } */
+
+  assert.deepEqual(capitalize(''), '');
+  assert.deepEqual(capitalize('hello'), 'Hello');
   
   console.log('Все тесты пройдены!');
